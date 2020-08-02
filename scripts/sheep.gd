92669fbbf337e6b6cdc99e695bd930ac89cb1c6a
obgamedev <69101998+obgamedev@users.noreply.github.com>
2020-08-02 12:14:12 +0300

    added environments

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -3,7 +3,7 @@ extends RigidBody
 # fly away
 func _on_Area_body_entered(body):
 	var dir = Vector3(sign(global_transform.origin.x - body.global_transform.origin.x), 1, sign(global_transform.origin.z - body.global_transform.origin.z))
-	apply_central_impulse(dir * 3)
+	apply_central_impulse(dir * 5)
 	apply_torque_impulse(dir)
 
 func _physics_process(delta):
