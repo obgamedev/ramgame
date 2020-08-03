66f293ab625a9727b60c113d6e135950fb30d0f5
koon-dev <kylienikka@seznam.cz>
2020-08-03 20:40:19 +0200

    rat juicing, ram idle fix

--- scripts/Rat.gd
+++ scripts/Rat.gd
@@ -1,6 +1,8 @@
 extends RigidBody
 
 var dir
+onready var TweenNode = get_node("Tween")
+onready var Smoked = get_node("Armature003/Skeleton/Cube009/CPUParticles")
 #copied from sheep code
 func _ready():
 	get_node("AnimationPlayer").play("MiceBWalkin")
@@ -10,6 +12,14 @@ func _on_Area_body_entered(body):
 	var dir = Vector3(sign(global_transform.origin.x - body.global_transform.origin.x), 1, sign(global_transform.origin.z - body.global_transform.origin.z))
 	apply_central_impulse(dir * 5)
 	apply_torque_impulse(dir)
+	get_node("AnimationPlayer").play("MiceCurled")
+	TweenNode.interpolate_property(self, "scale", null,  Vector3(3,3,3), 0.3, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	TweenNode.start()
+#	timer here pls
+#	TweenNode.interpolate_property(self, "scale", null,  Vector3(0,0,0), 0.3, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+#	TweenNode.start()
+	Smoked.set_emitting(true)
+	#sound
 	#imer here for despawn
 	pass # Replace with function body.
 
