94a3902a33f4a6b2429e2bef0b3e16fed271d203
obgamedev <69101998+obgamedev@users.noreply.github.com>
2020-08-11 19:57:38 +0300

    Revert "Merge branch 'master' into rainybranch"
    This reverts commit 4d816383d665437a1eaaf674c0e187f5f9acf6d4, reversing
    changes made to aef4744db27c8b69d39e0bcf0b914dde9ceaf86c.

--- scripts/Wolf.gd
+++ scripts/Wolf.gd
@@ -3,66 +3,34 @@ extends KinematicBody
 var SPEED = 10
 var INERTIA = 10
 var ACCELERATION = 2
-var MAX_HEALTH = 100
-enum {HUNT_STATE, HURT_STATE}
 
 var gravity = -9.8
 var velocity = Vector3()
 var flag = false
-var health = MAX_HEALTH
-var state = HUNT_STATE
-var timeRemaining = 0
-var hitVelocity
 
 onready var animationTree = get_node("AnimationTree")
-onready var mesh = get_node("wolf/Armature007/Skeleton/Cube015")
 
 func _physics_process(delta):
+	# move towards closest sheep
+	var distance = get_closest_sheep_coordinates() - global_transform.origin
+	distance.y = 0
+	var direction = distance.normalized()
+	var hv = Vector3()
+	hv.x = direction.x * SPEED
+	hv.z = direction.z * SPEED
+	hv.y = velocity.y + gravity * delta
 	
-	if state == HURT_STATE :
-		mesh.get_surface_material(0).albedo_color = Color.red if Engine.get_frames_drawn() % 2 == 0 else Color.white
-		hitVelocity *= 0.9
-		velocity = hitVelocity * SPEED * 10
-		velocity = move_and_slide(velocity, Vector3.UP, false, 4, PI / 4, false)
-		timeRemaining -= delta
-		
-		if timeRemaining <= 0 :
-			timeRemaining = 0
-			mesh.get_surface_material(0).albedo_color = Color.white
-			health -= 40
-			if health <= 0 :
-				queue_free()
-			state = HUNT_STATE
-	elif state == HUNT_STATE :
-		# move towards closest sheep
-		var distance = get_closest_sheep_coordinates() - global_transform.origin
-		distance.y = 0
-		var direction = distance.normalized()
-		var hv = Vector3()
-		hv.x = direction.x * SPEED
-		hv.z = direction.z * SPEED
-		hv.y = velocity.y + gravity * delta
-		
-		velocity = velocity.linear_interpolate(hv, ACCELERATION * delta)
-		velocity.y = hv.y
-		velocity = move_and_slide(velocity, Vector3.UP, false, 4, PI / 4, false)
-		rotation.y = atan2(direction.x, direction.z)
-		
-		# check if hit by ram
-		for i in get_slide_count() :
-			var collision = get_slide_collision(i)
-			if collision.collider.is_in_group("ram") :
-				hitVelocity = collision.normal
-				state = HURT_STATE
-				timeRemaining = 0.5
-				return
-		
-		# attack animation
-		if distance.length() < 13.5 && !flag :
-			flag = true
-			animationTree.set("parameters/my_one_shot/active",  true)
-		elif distance.length() >= 13.5 && flag :
-			flag = false
+	velocity = velocity.linear_interpolate(hv, ACCELERATION * delta)
+	velocity.y = hv.y
+	velocity = move_and_slide(velocity, Vector3.UP, false, 4, PI / 4, false)
+	rotation.y = atan2(direction.x, direction.z)
+	
+	# attack animation
+	if distance.length() < 13.5 && !flag :
+		flag = true
+		animationTree.set("parameters/my_one_shot/active",  true)
+	elif distance.length() >= 13.5 && flag :
+		flag = false
 
 func get_closest_sheep_coordinates():
 	var sheeps = get_tree().get_nodes_in_group("sheep")
--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -14,25 +14,20 @@ var health = MAX_HEALTH
 
 onready var animationTree = get_node("AnimationTree")
 onready var mesh = get_node("Armature002/Skeleton/Cube011")
-onready var HurtSound = $AudioHurt #for some reason, is late
-onready var healthBar : ProgressBar = get_node("Viewport/HealthBar")
-signal finished
 
 func _ready():
 	# set state and timer
 	rng.randomize()
 	state = IDLE_STATE
 	timeRemaining = rng.randf_range(0.5, 2)
-	healthBar.set("custom_styles/fg", healthBar.get("custom_styles/fg").duplicate())
 
 # hit by wolf
 func _on_Area_body_entered(body):
 	if state == HURT_STATE :
-		timeRemaining = 3
 		return
 	if !body.is_in_group("wolf") :
 		return
-	health -= 33.4
+	health -= 40
 	if health <= 0 :
 		state = DEAD_STATE
 	else :
@@ -58,9 +53,6 @@ func _physics_process(delta):
 	# blinking red (bug : effects all objects with the same material)
 	if state == HURT_STATE :
 		mesh.get_surface_material(0).albedo_color = Color.red if Engine.get_frames_drawn() % 2 == 0 else Color.white
-		# needs finished signal
-#		if _on_AudioHurt_finished() == true: 
-#			HurtSound.play()
 	
 	# decrement timer and check it
 	timeRemaining -= delta
@@ -85,20 +77,7 @@ func _physics_process(delta):
 	
 	# animation
 	animationTree.set("parameters/my_blend/blend_amount",  linear_velocity.length() / SPEED)
-	
-	# healthBar
-	healthBar.set_value(health)
-	var r = range_lerp(health, 10, 100, 1, 0)
-	var g = range_lerp(health, 10, 100, 0, 1)
-	var styleBox = healthBar.get("custom_styles/fg")
-	styleBox.bg_color = Color(r, g, 0)
 
 func set_velocity() :
 	velocity = dir * SPEED
 	linear_velocity = Vector3(velocity.x, 0, velocity.y)
-
-
-func _on_AudioHurt_finished():
-	if state == HURT_STATE:
-		HurtSound.play()
-	pass # Replace with function body.
