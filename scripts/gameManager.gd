33e728213322753ba9577c98436fab04862b2f8e
obgamedev <69101998+obgamedev@users.noreply.github.com>
2020-08-15 14:49:04 +0300

    added level timer

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -1,5 +1,10 @@
 extends Spatial
 
+var timePerLevel = 60;
+var timeElapsed = 0;
+
+onready var timerTextLabel = get_node("Timer/RichTextLabel")
+
 func _ready():
 	get_node("Barn/Cube021").set("health", Global.barnPercentage)
 	Global.wolfCount = 0
@@ -10,6 +15,14 @@ func _process(delta):
 		get_tree().change_scene("res://LoseScreen.tscn")
 	Global.barnPercentage = get_node("Barn/Cube021").get("health")
 	
+	# timer display
+	timeElapsed += delta
+	var timeLeft = timePerLevel - floor(timeElapsed)
+	
+	timerTextLabel.text = str(timeLeft)
+	
 	# trigger to go to the summery screen
-	if Input.is_key_pressed(KEY_ENTER) :
+	if timeLeft <= 0 :
 		get_tree().change_scene("res://Summary1.tscn")
+	
+	
