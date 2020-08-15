852a0bfb83b4e9382f8a2c4184ee882f10af4a93
koon-dev <kylienikka@seznam.cz>
2020-08-15 10:24:36 +0200

    added summary skeleton

---
+++ scripts/summarySetter.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var Barn = $Sprite/Barn
+onready var Rats = $Sprite/Rats
+onready var Wolvs = $Sprite/Wolvs
+func _ready():
+	pass # Replace with function body.
+
+#get the stored vars and put them in the boxes
+
+#func _process(delta):
+#	pass
